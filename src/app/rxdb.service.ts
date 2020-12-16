1f5df9ecc6c9e59d32db4a65e410a7580e467063
Andrew Davis <pulpdrew@gmail.com>
2020-12-16 15:49:40 -0500

    add rxdb

---
+++ src/app/rxdb.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { addRxPlugin, createRxDatabase, RxDatabase } from 'rxdb';
+import { RxDBValidatePlugin } from 'rxdb/plugins/validate';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RxdbService {
+
+  db: RxDatabase | null = null;
+
+  constructor() {
+    this.init();
+  }
+
+  private async init() {
+    addRxPlugin(require('pouchdb-adapter-indexeddb'));
+    addRxPlugin(RxDBValidatePlugin);
+
+    this.db = await createRxDatabase({
+      name: 'workout',
+      adapter: 'indexeddb',
+    });
+
+    await this.db.addCollections({
+      stuff: {
+        schema: {
+          properties: {
+            name: {
+              type: "string",
+              primary: true,
+            }
+          },
+          type: "object",
+          version: 0
+        }
+      }
+    });
+
+    await this.db.stuff.upsert({
+      name: "Andrew"
+    });
+
+    console.log((await this.db.stuff.find().exec()).map(d => d.name));
+  }
+}
